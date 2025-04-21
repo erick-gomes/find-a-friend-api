ac25c4b8014c42a6105903a5b661f8e43541cc9c
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-04-20 22:50:59 -0300

    feat: adicionar repositório OrgsRepository com métodos para criação e busca de organizações

---
+++ src/repositories/OrgsRepository.ts
@@ -0,0 +1,7 @@
+import { Org } from '@prisma/client'
+
+export interface OrgsRepository {
+  create(data: Org): Promise<Org>
+  findById(id: Pick<Org, 'id'>): Promise<Org | null>
+  findByEmail(email: Pick<Org, 'email'>): Promise<Org | null>
+}
